e185a19767f7e08591ed766751c2124ea1966b63
villageworks <villageworks@gmail.com>
2025-03-27 23:54:12 +0900

    ver.0.60:

--- src/main/java/com/example/demo/controller/RandomStringController.java
+++ src/main/java/com/example/demo/controller/RandomStringController.java
@@ -20,10 +20,11 @@ public class RandomStringController {
 	@PostMapping("/random")
 	public String generate(@RequestParam int charLength,                             // 生成文字数
 						   @RequestParam(defaultValue = "false") boolean withNumber, // 数字混在
+						   @RequestParam(name = "createCount") int count,            // 生成文字列数
 						   Model model // 遷移先にデータを引き継ぐスコープ
 						  ) {
 		// ランダム文字列生成サービスによる文字列リストの生成
-		List<String> stringList = service.generate(charLength, withNumber);
+		List<String> stringList = service.generate(charLength, withNumber, count);
 		// 生成した文字列リストをスコープに登録
 		model.addAttribute("randList", stringList);
 		// 自画面遷移
--- src/main/java/com/example/demo/service/RandomStringService.java
+++ src/main/java/com/example/demo/service/RandomStringService.java
@@ -18,14 +18,15 @@ public class RandomStringService {
 	 * ランダムな文字列のリストを生成する
 	 * @param charLength ひとつの文字列の文字数
 	 * @param withNumber 数字を含める場合はtrue、それ以外はfalse
+	 * @param count 
 	 * @return ランダムな文字列のリスト
 	 */
 	@SuppressWarnings("deprecation") // TODO: 非推奨メソッドを検討する！
-	public List<String> generate(int charLength, boolean withNumber) {
+	public List<String> generate(int charLength, boolean withNumber, int count) {
 		// 生成した文字列を格納する文字列リストの初期化
 		List<String> list = new ArrayList<>();
 		// 生成する個数分の繰り返し
-		for (int i = 0; i < GENERATE_NUM; i++) {
+		for (int i = 0; i < count; i++) {
 			if (withNumber) {
 				// 数字を含む文字列を文字列リストに追加
 				list.add(RandomStringUtils.randomAlphanumeric(charLength));
